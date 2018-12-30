95eaacd666d1e01eb269ca6423f6ed00783e86cd
Ian Harrigan <ianharrigan@hotmail.com>
2018-12-30 17:18:57 +0100

    demo

--- tabviews/src/Main.hx
+++ tabviews/src/Main.hx
@@ -1,6 +1,8 @@
 package ;
 
 import haxe.ui.HaxeUIApp;
+import haxe.ui.components.Button;
+import haxe.ui.components.TabBar2;
 import haxe.ui.core.Component;
 import haxe.ui.macros.ComponentMacros;
 
@@ -11,6 +13,20 @@ class Main {
             var main:Component = ComponentMacros.buildComponent("assets/main.xml");
             app.addComponent(main);
 
+            var tabs = new TabBar2();
+            tabs.percentWidth = 100;
+            tabs.tabPosition = "bottom";
+            
+            var b = new Button();
+            b.text = "Page 1";
+            tabs.addComponent(b);
+            
+            var b = new Button();
+            b.text = "Page 2";
+            tabs.addComponent(b);
+            
+            main.addComponent(tabs);
+            
             app.start();
         });
     }
