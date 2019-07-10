007749c8e6dcfac8d7178a9b9d5a4bed18415fad
Ian Harrigan <ianharrigan@hotmail.com>
2019-07-10 09:03:33 +0200

    dialogs

--- dialogs/src/Main.hx
+++ dialogs/src/Main.hx
@@ -4,9 +4,8 @@ import haxe.ui.HaxeUIApp;
 import haxe.ui.Toolkit;
 import haxe.ui.components.Button;
 import haxe.ui.components.Image;
-import haxe.ui.components.Label;
 import haxe.ui.containers.dialogs.Dialog;
-import haxe.ui.containers.dialogs.MessageBox;
+import haxe.ui.containers.dialogs.Dialog.DialogButton;
 import haxe.ui.containers.dialogs.MessageBox.MessageBoxType;
 import haxe.ui.core.Component;
 import haxe.ui.macros.ComponentMacros;
@@ -27,21 +26,21 @@ class Main {
 
             var questionButton = main.findComponent("questionButton", Button);
             questionButton.onClick = function(e) {
-                Toolkit.messageBox("This is a test question message\n\nLine1\nLine2\nLine3", MessageBoxType.QUESTION, function(button) {
+                Toolkit.messageBox("This is a test question message\n\nLine1\nLine2\nLine3", MessageBoxType.TYPE_QUESTION, function(button) {
                     trace("'" + button + "' clicked");
                 });
             };
             
             var warningButton = main.findComponent("warningButton", Button);
             warningButton.onClick = function(e) {
-                Toolkit.messageBox("This is a test warning message\n\nLine1\nLine2\nLine3", MessageBoxType.WARNING, function(button) {
+                Toolkit.messageBox("This is a test warning message\n\nLine1\nLine2\nLine3", MessageBoxType.TYPE_WARNING, function(button) {
                     trace("'" + button + "' clicked");
                 });
             };
             
             var errorButton = main.findComponent("errorButton", Button);
             errorButton.onClick = function(e) {
-                Toolkit.messageBox("This is a test error message\n\nLine1\nLine2\nLine3", MessageBoxType.ERROR, function(button) {
+                Toolkit.messageBox("This is a test error message\n\nLine1\nLine2\nLine3", MessageBoxType.TYPE_ERROR, function(button) {
                     trace("'" + button + "' clicked");
                 });
             };
@@ -66,6 +65,34 @@ class Main {
                     trace("'" + button + "' clicked");
                 });
             }
+            
+            var test = main.findComponent("test", Button);
+            test.onClick = function(e) {
+                var dialog = new Dialog();
+                dialog.buttons = DialogButton.CLOSE;
+                var button = new Button();
+                button.percentWidth = 100;
+                button.percentHeight = 100;
+                dialog.title = "Test";
+                button.text = "Dialog Button";
+                dialog.addComponent(button);
+                dialog.width = 300;
+                dialog.height = 300;
+                dialog.show();
+            };
+            
+            var test2 = main.findComponent("test2", Button);
+            test2.onClick = function(e) {
+                var dialog = new Dialog();
+                dialog.buttons = DialogButton.CLOSE;
+                var button = new Button();
+                button.percentWidth = 100;
+                dialog.title = "Test 2";
+                button.text = "Dialog Button";
+                dialog.addComponent(button);
+                dialog.width = 300;
+                dialog.show();
+            };
             
             app.start();
         });
