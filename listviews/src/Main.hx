c7ef18c494b1e93d3c65048680a4e157f2231ee4
Ian Harrigan <ianharrigan@hotmail.com>
2019-12-16 19:05:05 +0100

    samples

--- listviews/src/Main.hx
+++ listviews/src/Main.hx
@@ -1,7 +1,12 @@
 package ;
 
 import haxe.ui.HaxeUIApp;
+import haxe.ui.components.Button;
+import haxe.ui.containers.HBox;
+import haxe.ui.containers.ListView;
 import haxe.ui.core.Component;
+import haxe.ui.core.ItemRenderer;
+import haxe.ui.events.ItemEvent;
 import haxe.ui.macros.ComponentMacros;
 
 class Main {
@@ -11,6 +16,17 @@ class Main {
             var main:Component = ComponentMacros.buildComponent("assets/main.xml");
             app.addComponent(main);
 
+            var button = main.findComponent("bob2", ItemRenderer);
+            if (button != null) {
+                var b = button.cloneComponent();
+                main.addComponent(b);
+            }
+            
+            var lv = main.findComponent("lv2", ListView);
+            lv.onComponentEvent = function(e) {
+                trace(e.data);
+            }
+            
             app.start();
         });
     }
