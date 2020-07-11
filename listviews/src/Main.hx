6e1e9ba9e8c93c9f066ef11a879018d0e0f44e6d
Ian Harrigan <ianharrigan@hotmail.com>
2020-07-11 18:54:01 +0200

    clean up

--- listviews/src/Main.hx
+++ listviews/src/Main.hx
@@ -16,17 +16,6 @@ class Main {
             var main:Component = ComponentMacros.buildComponent("assets/main.xml");
             app.addComponent(main);
 
-            var button = main.findComponent("bob2", ItemRenderer);
-            if (button != null) {
-                var b = button.cloneComponent();
-                main.addComponent(b);
-            }
-            
-            var lv = main.findComponent("lv2", ListView);
-            lv.onComponentEvent = function(e) {
-                trace(e.data);
-            }
-            
             app.start();
         });
     }
