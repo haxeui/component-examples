45817b4296ea9539d482c977421672c3fb77d117
Ian Harrigan <ianharrigan@hotmail.com>
2020-07-12 07:53:30 +0200

    splitter example

---
+++ splitters/src/Main.hx
@@ -0,0 +1,17 @@
+package ;
+
+import haxe.ui.HaxeUIApp;
+import haxe.ui.core.Component;
+import haxe.ui.macros.ComponentMacros;
+
+class Main {
+    public static function main() {
+        var app = new HaxeUIApp();
+        app.ready(function() {
+            var mainView:Component = ComponentMacros.buildComponent("assets/main-view.xml");
+            app.addComponent(mainView);
+
+            app.start();
+        });
+    }
+}
